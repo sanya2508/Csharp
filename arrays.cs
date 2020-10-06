40c41269f3fd6f5e87afbd7cbecd0749099a1986
Sanya Kumari <1701sanya@gmail.com>
2020-10-06 22:17:59 +0530

    9

---
+++ arrays.cs
@@ -0,0 +1,23 @@
+using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Array
+            int[] luckyNumbers = { 1, 3, 7, 11 };
+            Console.WriteLine(luckyNumbers[2]);
+            luckyNumbers[0] = 21;
+
+            // we need to tell the number of elements
+            string[] friends = new string[100];
+            friends[0] = Apples;
+            friends[1] = Car;
+
+
+            Console.ReadLine();  
+        }
+    }
+}
