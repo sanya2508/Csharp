b2657ad4671b3f69668341ca7022120fb7153008
Sanya Kumari <1701sanya@gmail.com>
2020-10-07 00:55:01 +0530

    Create 2d array.cs

---
+++ 2d array.cs
@@ -0,0 +1,20 @@
+using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] number =
+            {
+                {1,2 },
+                {3,4 },
+                {5,6 }
+            };
+
+            Console.WriteLine(number[1, 1]);
+        }
+            
+    }
+}
