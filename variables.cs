3b392a2e91f0c835c0aa895f11ae6bc33515f820
Sanya Kumari <1701sanya@gmail.com>
2020-10-06 14:11:43 +0530

    3

---
+++ variables.cs
@@ -0,0 +1,23 @@
+using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string charName = "Sanya";
+            int charAge = 20;
+            Console.WriteLine("Name " + charName);
+            Console.WriteLine("Age " + charAge);
+
+            charName = "ABC";
+            charAge = 18;
+            Console.WriteLine("Name " + charName);
+            Console.WriteLine("Age " + charAge);
+
+            Console.ReadLine();
+           
+        }
+    }
+}
