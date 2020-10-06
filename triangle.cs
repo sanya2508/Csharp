083180fbac5b9fc4d8c355ddc0cec0f58e16852e
Sanya Kumari <1701sanya@gmail.com>
2020-10-06 13:58:31 +0530

    Second code

---
+++ triangle.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("    /|");
+            Console.WriteLine("   / |");
+            Console.WriteLine("  /  |");
+            Console.WriteLine(" /___|");
+
+
+        }
+    }
+}
