e3ed71cf3977f977ef06d412e32bf13a393f01cb
Sanya Kumari <1701sanya@gmail.com>
2020-10-06 14:16:26 +0530

    Create common datatypes.cs

---
+++ common datatypes.cs
@@ -0,0 +1,23 @@
+using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string phrase = "Eat sleep";
+            char grade = 'A';
+            int age = 30;
+
+            // less to most precise:
+            // float, double, decimal
+
+            double gpa = 3.2;
+            bool isDead = false; 
+
+            Console.ReadLine();
+           
+        }
+    }
+}
